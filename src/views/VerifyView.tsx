5408c272161c69b97d8eca76979ab79bb27d25c0
Edson Alcala <edson_alcala@msn.com>
2020-05-23 17:18:03 +0100

    removed inputs for fields

--- src/views/VerifyView.tsx
+++ src/views/VerifyView.tsx
@@ -7,7 +7,7 @@ interface Props {
 }
 
 export const VerifyView: React.FC<Props> = ({ onEditClick }) => {
-    const onVerifyContract = () => {
+    const onVerifyContract = (values: any) => { // TODO
         console.log("On Verify Contract Clicked")
         // TODO
     }
@@ -28,19 +28,11 @@ export const VerifyView: React.FC<Props> = ({ onEditClick }) => {
                 }
                 return errors;
             }}
-            onSubmit={(values) => {
-                console.log("Values", values)
-                // TODO
-            }}>
+            onSubmit={(values) => onVerifyContract(values)}>
             {({ errors, touched, handleSubmit }) => (
                 <form onSubmit={handleSubmit}>
                     <div style={{ float: "right" }}>
-                        <svg onClick={() => {
-                            console.log("Icon clicked")
-                            // Set is in EditMode
-
-                            onEditClick()
-                        }} className="bi bi-gear-fill" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                        <svg onClick={() => onEditClick()} className="bi bi-gear-fill" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                             <path fillRule="evenodd" d="M9.405 1.05c-.413-1.4-2.397-1.4-2.81 0l-.1.34a1.464 1.464 0 0 1-2.105.872l-.31-.17c-1.283-.698-2.686.705-1.987 1.987l.169.311c.446.82.023 1.841-.872 2.105l-.34.1c-1.4.413-1.4 2.397 0 2.81l.34.1a1.464 1.464 0 0 1 .872 2.105l-.17.31c-.698 1.283.705 2.686 1.987 1.987l.311-.169a1.464 1.464 0 0 1 2.105.872l.1.34c.413 1.4 2.397 1.4 2.81 0l.1-.34a1.464 1.464 0 0 1 2.105-.872l.31.17c1.283.698 2.686-.705 1.987-1.987l-.169-.311a1.464 1.464 0 0 1 .872-2.105l.34-.1c1.4-.413 1.4-2.397 0-2.81l-.34-.1a1.464 1.464 0 0 1-.872-2.105l.17-.31c.698-1.283-.705-2.686-1.987-1.987l-.311.169a1.464 1.464 0 0 1-2.105-.872l-.1-.34zM8 10.93a2.929 2.929 0 1 0 0-5.86 2.929 2.929 0 0 0 0 5.858z" />
                         </svg>
                     </div>
@@ -52,14 +44,6 @@ export const VerifyView: React.FC<Props> = ({ onEditClick }) => {
                             type="text"
                             name="contractName"
                             placeholder="i.e. Ballot" />
-                        {/* <input
-                            className={(errors.contractName && touched.contractName) ? "form-control form-control-sm is-invalid" : "form-control form-control-sm"}
-                            type="text"
-                            onChange={handleChange}
-                            onBlur={handleBlur}
-                            value={values.contractName}
-                            name="contractName"
-                            placeholder="i.e. Ballot" /> */}
                         <ErrorMessage className="invalid-feedback" name="contractName" component="div" />
                     </div>
 
@@ -70,14 +54,6 @@ export const VerifyView: React.FC<Props> = ({ onEditClick }) => {
                             type="text"
                             name="contractArguments"
                             placeholder="hex encoded" />
-                        {/* <input
-                            className={(errors.contractArguments && touched.contractArguments) ? "form-control form-control-sm is-invalid" : "form-control form-control-sm"}
-                            type="text"
-                            onChange={handleChange}
-                            onBlur={handleBlur}
-                            value={values.contractArguments}
-                            name="contractArguments"
-                            placeholder="hex encoded" /> */}
                         <ErrorMessage className="invalid-feedback" name="contractArguments" component="div" />
                     </div>
 
@@ -88,14 +64,6 @@ export const VerifyView: React.FC<Props> = ({ onEditClick }) => {
                             type="text"
                             name="contractAddress"
                             placeholder="i.e. 0x11b79afc03baf25c631dd70169bb6a3160b2706e" />
-                        {/* <input
-                            className={(errors.contractAddress && touched.contractAddress) ? "form-control form-control-sm is-invalid" : "form-control form-control-sm"}
-                            type="text"
-                            onChange={handleChange}
-                            onBlur={handleBlur}
-                            value={values.contractAddress}
-                            name="contractAddress"
-                            placeholder="i.e. 0x11b79afc03baf25c631dd70169bb6a3160b2706e" /> */}
                         <ErrorMessage className="invalid-feedback" name="contractAddress" component="div" />
                     </div>
 
