7f5024da59daf966ba6d833f5ba2cad7d1e06607
Edson Alcala <edson_alcala@msn.com>
2020-05-24 08:03:47 +0100

    fix test and caching

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,7 +4,7 @@ import App from "./App"
 
 test("renders learn react link", () => {
   const { getByText } = render(<App />)
-  const linkElement = getByText(/learn react/i)
+  const linkElement = getByText(/Please Enter your API key/i)
 
   expect(linkElement).toBeInTheDocument()
 })
